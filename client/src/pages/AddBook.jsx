9f0825d3d1a0951a3aba93079d8611849e5b52af
Geuse Betel <ju-shapina@yandex.ru>
2021-11-18 20:44:54 +0300

    ffsdfsdf

--- client/src/pages/AddBook.jsx
+++ client/src/pages/AddBook.jsx
@@ -35,6 +35,12 @@ function AddBook() {
         setInfo(info.map(i => i.number === number ? { ...i, [key]: value } : i))
     }
 
+    var array1 = ['a', 'b', 'c'];
+    var technologyList = [];
+    array1.forEach(function (element) {
+        technologyList.push({ label: element, value: element })
+    });
+
 
     return (
 
@@ -49,72 +55,90 @@ function AddBook() {
                 <FormGroup className="mb-3" controlId="bookname">
                     <Form.Control required type="text" placeholder="Название книги" />
                 </FormGroup>
-                <Row className="g-2">
-                    <Col md>
-                        <FormGroup className="mb-3" controlId="authors">
-                            <Form.Control type="text" placeholder="Авторы" />
-                        </FormGroup>
-                    </Col>
-                    <Col md>
-                        <FormGroup controlId="authselect" className="mb-3">
-                            <Form.Select >
+                <Row className="mb-3 mx-1">
 
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </Form.Select>
-                        </FormGroup>
-                    </Col>
-                    <Col md>
-                        <Button id="authadd" variant="secondary" type="input">
-                            +
-                        </Button>
-                    </Col>
+
+                    <Button id="authadd" variant="secondary" onClick={addInfo}>
+                        Добавить авторов
+                    </Button>
+                    {info.map(i =>
+                        <Row className="my-2" key={i.number}>
+                            <Col w-400>
+                                <Form.Select onChange={(e) => changeInfo('title', e.target.value, i.number)}>
+                                   {technologyList}                                   
+                                </Form.Select>
+                            </Col>
+                            <Col>
+                                <Button
+                                    onClick={() => removeInfo(i.number)}
+                                    variant={"secondary"}
+                                >
+                                    -
+                                </Button>
+                            </Col>
+                        </Row>
+                    )}
+
+
 
                 </Row>
-                <Row className="g-2">
-                    <Col md>
-                        <FormGroup className="mb-3" controlId="tags">
-                            <Form.Control type="text" placeholder="Теги" />
-                        </FormGroup>
-                    </Col>
-                    <Col md>
-                        <FormGroup controlId="tagselect" className="mb-3">
-                            <Form.Select >
+                <Row className="mb-3 mx-1">
 
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </Form.Select>
-                        </FormGroup>
-                    </Col>
-                    <Col md>
-                        <Button id="tagadd" variant="secondary" type="input">
-                            +
-                        </Button>
-                    </Col>
+
+                    <Button id="authadd" variant="secondary" onClick={addInfo}>
+                        Добавить теги
+                    </Button>
+                    {info.map(i =>
+                        <Row className="my-2" key={i.number}>
+                            <Col w-400>
+                                <Form.Select onChange={(e) => changeInfo('title', e.target.value, i.number)}>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </Form.Select>
+                            </Col>
+                            <Col>
+                                <Button
+                                    onClick={() => removeInfo(i.number)}
+                                    variant={"secondary"}
+                                >
+                                    -
+                                </Button>
+                            </Col>
+                        </Row>
+                    )}
+
+
+
                 </Row>
-                <Row className="g-2">
-                    <Col md>
-                        <FormGroup className="mb-3" controlId="series">
-                            <Form.Control type="text" placeholder="Серии" />
-                        </FormGroup>
-                    </Col>
-                    <Col md>
-                        <FormGroup controlId="serselect" className="mb-3">
-                            <Form.Select >
+                <Row className="mb-3 mx-1">
 
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </Form.Select>
-                        </FormGroup>
-                    </Col>
-                    <Col md>
-                        <Button id="seradd" variant="secondary" type="input">
-                            +
-                        </Button>
-                    </Col>
+
+                    <Button id="authadd" variant="secondary" onClick={addInfo}>
+                        Добавить серии
+                    </Button>
+                    {info.map(i =>
+                        <Row className="my-2" key={i.number}>
+                            <Col w-400>
+                                <Form.Select onChange={(e) => changeInfo('title', e.target.value, i.number)}>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </Form.Select>
+                            </Col>
+                            <Col>
+                                <Button
+                                    onClick={() => removeInfo(i.number)}
+                                    variant={"secondary"}
+                                >
+                                    -
+                                </Button>
+                            </Col>
+                        </Row>
+                    )}
+
+
+
                 </Row>
 
                 <Form.Group className="mb-3" controlId="BookDescr">
@@ -133,7 +157,7 @@ function AddBook() {
 
                 {info.map(i =>
                     <Row className="mt-4" key={i.number}>
-                        <Col md={4}>                            
+                        <Col md={4}>
                             <Form.Select onChange={(e) => changeInfo('title', e.target.value, i.number)}>
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
@@ -157,48 +181,6 @@ function AddBook() {
 
             </Form>
 
-            {/*<input type="text" placeholder="ISBN" />
-            <input type="text" placeholder="Название книги" />
-            <div>
-
-
-                <Input value={tags.toString()} placeholder="автор(ы)" />
-                <select name="authsel" id="authsel" value={tag} onChange={e => setTag(e.target.value)} >
-                    <option value="автор1">автор1</option>
-                    <option value="автор2">автор2</option>
-                    <option value="автор3">автор3</option>
-                    <option value="автор4">автор4</option>
-                </select>
-                
-                <button onClick={gettag} onChange>+</button>
-            </div>
-            <div>
-                <input type="text" name="taginp" id="taginp" placeholder="теги" />
-                <select name="tagsel" id="tagsel">
-                    <option value="тег1" >тег1</option>
-                    <option value="тег2">тег2</option>
-                    <option value="тег3">тег3</option>
-                    <option value="тег4">тег4</option>
-
-                </select>
-                <button onclick={addtag}>+</button>
-            </div>
-
-            <div>
-                <input type="text" name="serinp" id="serinp" placeholder="серия" />
-                <select name="sersel" id="sersel">
-                    <option value="серия1">серия1</option>
-                    <option value="серия2">серия2</option>
-                    <option value="серия3">серия3</option>
-                    <option value="серия4">серия4</option>
-
-                </select>
-                <button onclick="addtag()">+</button>
-            </div>
-
-            <textarea placeholder="описание" rows='10'></textarea>
-
-            <button>вхерачить</button>*/}
 
         </div>
 
