d4273604825218d3fd65a04b9aee50ec7eed25e6
ted Afflic <Afflicted_1996@mail.ru>
2021-11-19 01:05:27 +0300

    books fix

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -5,6 +5,7 @@ import { useBooks } from "../components/hooks/useBooks";
 import '../styles/App.css'
 import axios from 'axios'
 import { useHttp } from "../components/hooks/html.hook";
+import { Loader } from "../components/UI/Loader";
 
 function Books() {
     const [Books, setBook] = useState([]) 
@@ -16,27 +17,54 @@ function Books() {
 
     useEffect(() => {
       //fetchPosts()
-      getAllBook()
+      //getAllBook()
+      fetchBooks()
     },[]) 
 
   //  const[filter,setFilter]=useState({sort:'',query:''})
 
   //  const sortedAndSearchedBooks = useBooks(Books, filter.sort, filter.query)
     
-    const {loading, error, request}= useHttp()
-
+   // const {loading, error, request}= useHttp()
+/*
     const getAllBook = async () =>{
       try {
         const data = await request('api/book')
         console.log('Data',data)
         setBook(data)
       } catch (error) {}
-    }
+    }*/
+
+    
+
+    const [loading, setLoading] = useState(true)
+  
+
+  async function fetchBooks(){  
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}api/book`)
+    
+    setBook(response.data)
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchBooks().finally(() => setLoading(false))
+    }, 1000);
+  }, [])
+
+
+
+
+
 
     
   //  <Filter filter={filter} setFilter={setFilter}></Filter>   
+  if (loading) {
+    return <Loader/>
+  } 
   return(
-  <div className="App">     
+  <div className="App"> 
+       
     <BookList Books={Books}/>   
     
     
