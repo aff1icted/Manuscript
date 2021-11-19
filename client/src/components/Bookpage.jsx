e2d6b8a7e2ff6e9e57022a7c24587890d7fa6e65
ted Afflic <Afflicted_1996@mail.ru>
2021-11-19 08:10:52 +0300

    addbook books

--- client/src/components/Bookpage.jsx
+++ client/src/components/Bookpage.jsx
@@ -1,28 +1,31 @@
 import e from "cors";
-import react, { useState , useEffect} from "react"
-import { useHistory} from "react-router";
+import react, { useState, useEffect } from "react"
+import { useHistory } from "react-router";
 import AuthorList from "./AuthorList";
 import { Image } from "react-bootstrap";
 import TagList from "./TagList";
-//<button onClick={getOneBook} disabled={loading}>get</button>
+
 const BookPage = (props) => {
   const router = useHistory()
   console.log('Book', props)
-  console.log('tag', props.Book.tags)   
-  console.log('art', process.env.REACT_APP_API_URL+props.Book.coverart)  
+  console.log('tag', props.Book.tags)
+  console.log('art', process.env.REACT_APP_API_URL + props.Book.coverart)
   return (
     <div class="Bookdet">
       <div className="upperbook">
 
 
         {/*<Image  width={100}height={200}scr={process.env.REACT_APP_API_URL+props.Book.coverart}/>*/}
-        <img scr={process.env.REACT_APP_API_URL+props.Book.coverart} />
+        <img scr={process.env.REACT_APP_API_URL + props.Book.coverart} />
         <div className="infosdet">
+          
           <div>{props.Book.title}</div>
-          <div>{props.Book.Authors}</div>
-          <TagList tags={props.Book.tags}/>
-          <AuthorList authors={props.Book.authors}/>
-          <div>{/*props.Book.tags.map(tag =>tag.tagname)props.Book.tags[0].tagname*/}</div>
+          <div>{props.Book.isbn}</div>
+          <TagList tags={props.Book.tags} />
+          <AuthorList authors={props.Book.authors} />
+          <div>Дата публикации:{props.Book.publicationdate}</div>
+          <div>количество страниц:{props.Book.pagenumber}</div>
+          <div>тираж:{props.Book.edition}</div>
           <div className="description">{props.Book.description}</div>
         </div>
       </div>
