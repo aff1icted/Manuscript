66541456e162e6d2043671d0de94db15c45ab4fe
ted Afflic <Afflicted_1996@mail.ru>
2022-01-27 10:29:51 +0300

    serv sett2

--- app.js
+++ app.js
@@ -19,13 +19,6 @@ app.use(fileUpload({}))
 app.use('/api',router)
 app.use(errorHandling)
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-  }
 
 const start = async () =>{
     try {
