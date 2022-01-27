093f5e593d08483ba680d7b9846318ded34f1ed2
ted Afflic <Afflicted_1996@mail.ru>
2022-01-27 09:48:32 +0300

    serv sett

--- app.js
+++ app.js
@@ -16,12 +16,16 @@ app.use(cors())
 app.use(express.json())/*{extended:true} */
 app.use(express.static(path.resolve(__dirname,'static')))
 app.use(fileUpload({}))
-
 app.use('/api',router)
-
-
-
 app.use(errorHandling)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  }
 
 const start = async () =>{
     try {
