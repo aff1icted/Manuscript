25cf7ca384edb25db2b06cbb7557a5e5cc093624
ted Afflic <Afflicted_1996@mail.ru>
2022-03-20 09:51:28 +0300

    ASD

--- controller/tag.controller.js
+++ controller/tag.controller.js
@@ -1,24 +1,24 @@
-const {Type, Tags} = require ('../models/models')
+const { Type, Tags } = require('../models/models')
 const ApiError = require('../error/ApiError');
 
-class TagController{
-    async  create(req, res, next){
-        const {tagname} = req.body
-        const candidate = await Tags.findOne({where: {tagname}})
+class TagController {
+    async create(req, res, next) {
+        const { tagname } = req.body
+        const candidate = await Tags.findOne({ where: { tagname } })
         if (candidate) {
             return next(ApiError.badRequest('Такой тег уже существует'))
         }
-        const tag = await Tags.create({tagname})
-        return res.json({tag})
+        const tag = await Tags.create({ tagname })
+        return res.json({ tag })
     }
 
-    async getAll(req, res){
+    async getAll(req, res) {
         const tags = await Tags.findAll()
         return res.json(tags)
     }
 
     async update(req, res) {
-        
+
     }
 
     async delete(req, res) {
